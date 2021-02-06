7735b83eb8973bf4827a51cee1ffb7e012bb1743
justmdiaz <justmdiaz@knights.ucf.edu>
2021-02-06 18:04:02 -0500

    Select Screen Scene

---
+++ Gauntlet/Assets/Scripts/selectorScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class selectorScript : MonoBehaviour
+{
+    private GameObject[] characterList;
+    private int index; 
+
+    private void Start()
+    {
+        characterList = new GameObject[transform.childCount];
+
+        for(int i = 0; i < transform.childCount; i++)
+            characterList[i] = transform.GetChild(i).gameObject;
+
+        foreach (GameObject go in characterList)
+            go.SetActive(false);
+
+        if (characterList[0])
+            characterList[0].SetActive(true);
+        
+    }
+
+    
+
+    public void ToggleBerserker()
+    {
+        characterList[index].SetActive(false);
+
+
+        characterList[0].SetActive(true);
+        index = 0;
+    }
+    public void ToggleMage()
+    {
+        characterList[index].SetActive(false);
+
+
+        characterList[1].SetActive(true);
+        index = 1;
+
+
+    }
+    public void ToggleArcher()
+    {
+        characterList[index].SetActive(false);
+
+
+        characterList[2].SetActive(true);
+        index = 2;
+
+
+    }
+    public void TogglePaladin()
+    {
+        characterList[index].SetActive(false);
+
+
+        characterList[3].SetActive(true);
+        index = 3;
+    }
+}
